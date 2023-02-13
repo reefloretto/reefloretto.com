a9254b23aba0288361784ac3643f91f533917d48
Reef Loretto <reefloretto@gmail.com>
2023-02-13 17:51:54 +0000

    wip

--- untitled1994/src/app/layout.tsx
+++ untitled1994/src/app/layout.tsx
@@ -1,15 +1,16 @@
-import './globals.css'
+import "./globals.css";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <head />
       <body>
-        <main>{children}</main></body>
+        <main>{children}</main>
+      </body>
     </html>
-  )
+  );
 }
