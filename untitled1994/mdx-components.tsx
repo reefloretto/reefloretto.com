d53ff185bb233836bef18311c6d38d407bab0332
Reef Loretto <reefloretto@gmail.com>
2023-02-13 19:14:07 +0000

    hii

--- untitled1994/mdx-components.tsx
+++ untitled1994/mdx-components.tsx
@@ -2,7 +2,7 @@ export function useMDXComponents(components: {
   [component: string]: React.ComponentType;
 }) {
   return {
-    h1: ({ children }) => <h1 className="text-3xl">{children}</h1>,
+    h1: ({ children }) => <h1 className="text-3xl mb-2">{children}</h1>,
     p: ({ children }) => <p className="mb-1">{children}</p>,
     ...components,
   };
--- untitled1994/tailwind.config.js
+++ untitled1994/tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{md,mdx,js,ts,jsx,tsx}",
+    "./mdx-components.tsx",
+    "./src/**/*.{js,ts,jsx,tsx,md,mdx}",
   ],
   theme: {
     extend: {
