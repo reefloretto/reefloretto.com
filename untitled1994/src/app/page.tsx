55b8ed28ba67650fa76ed8ce910439d81faf0503
Reef Loretto <reefloretto@gmail.com>
2023-02-13 20:16:54 +0000

    more

--- untitled1994/src/app/page.tsx
+++ untitled1994/src/app/page.tsx
@@ -2,7 +2,7 @@ import IndexContent from "./mdx/index.mdx";
 
 export default function Page() {
   return (
-    <div className="container mt-7 max-w-3xl">
+    <div className="container mt-10 max-w-3xl">
       <IndexContent />
     </div>
   );
