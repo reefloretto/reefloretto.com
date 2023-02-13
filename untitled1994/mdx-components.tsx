b2235c45bf1a59b52f8e8f8d28b242419eca75b3
Reef Loretto <reefloretto@gmail.com>
2023-02-13 18:59:44 +0000

    add

--- untitled1994/mdx-components.tsx
+++ untitled1994/mdx-components.tsx
@@ -1,10 +1,8 @@
-// This file is required to use MDX in `app` directory.
 export function useMDXComponents(components: {
   [component: string]: React.ComponentType;
 }) {
   return {
-    // Allows customizing built-in components, e.g. to add styling.
-    // h1: ({ children }) => <h1 style={{ fontSize: "100px" }}>{children}</h1>,
+    h1: ({ children }) => <h1 className="text-3xl">{children}</h1>,
     p: ({ children }) => <p className="mb-1">{children}</p>,
     ...components,
   };
