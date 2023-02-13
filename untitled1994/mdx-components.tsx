e428ced5ad6224235cd4e7e465bf5da27b976b74
Reef Loretto <reefloretto@gmail.com>
2023-02-13 19:25:55 +0000

    ts

--- untitled1994/mdx-components.tsx
+++ untitled1994/mdx-components.tsx
@@ -2,8 +2,8 @@ export function useMDXComponents(components: {
   [component: string]: React.ComponentType;
 }) {
   return {
-    h1: ({ children }) => <h1 className="text-3xl mb-2">{children}</h1>,
-    p: ({ children }) => <p className="mb-1">{children}</p>,
+    h1: ({ children }: any) => <h1 className="text-3xl mb-2">{children}</h1>,
+    p: ({ children }: any) => <p className="mb-1">{children}</p>,
     ...components,
   };
 }
