5627eabeaf2c2b6f87a0b3c02cf6bfc1dbd2e1eb
Reef Loretto <reefloretto@gmail.com>
2023-02-14 01:08:57 +0000

    color

--- untitled1994/src/app/page.tsx
+++ untitled1994/src/app/page.tsx
@@ -2,8 +2,10 @@ import IndexContent from "./mdx/index.mdx";
 
 export default function Page() {
   return (
-    <div className="container mt-10 max-w-3xl">
-      <IndexContent />
+    <div className="w-screen h-screen bg-slate-50">
+      <div className="text-stone-900 container pt-10 max-w-3xl">
+        <IndexContent />
+      </div>
     </div>
   );
 }
