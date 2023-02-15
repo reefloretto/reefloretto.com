b5e405775dc81ffa144bb70f84bd8c1739a9df46
Reef Loretto <reefloretto@gmail.com>
2023-02-15 02:30:49 +0000

    okaymore

--- untitled1994/mdx-components.tsx
+++ untitled1994/mdx-components.tsx
@@ -4,6 +4,10 @@ export function useMDXComponents(components: {
   return {
     h1: ({ children }: any) => <h1 className="text-3xl">{children}</h1>,
     p: ({ children }: any) => <p>{children}</p>,
+    pre: ({ children }: any) => <pre className="my-8">{children}</pre>,
+    code: ({ children }: any) => (
+      <code className="bg-slate-200 p-3">{children}</code>
+    ),
     img: ({ children }: any) => (
       <img className="object-contain aspect-auto relative w-7" src={children} />
     ),
