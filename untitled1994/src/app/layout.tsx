a4e4cee0b3fc4c86267567f90e51533dd0f35a5b
Reef Loretto <reefloretto@gmail.com>
2023-02-15 19:25:32 +0000

    okay

--- untitled1994/src/app/layout.tsx
+++ untitled1994/src/app/layout.tsx
@@ -17,9 +17,9 @@ export default function RootLayout({
         <div className="container mt-4 max-w-3xl">
           <div className="text-neutral-900">
             <Link className={`${linkHoverClassName}`} href="/">
-              <h1 className="text-3xl">Reef Loretto</h1>
+              <h1 className="font-bold text-3xl">Reef Loretto</h1>
             </Link>
-            <div className="text-neutral-600 my-4 flex flex-row space-x-3">
+            <div className="font-extralight text-neutral-600 my-4 flex flex-row space-x-3">
               <Link className={`rounded ${linkHoverClassName}`} href="/">
                 Home
               </Link>
@@ -58,7 +58,7 @@ export default function RootLayout({
               />
             </Link>
           </div>
-          <footer className="text-sm text-neutral-500 my-4">
+          <footer className="font-extralight text-sm text-neutral-500 my-4">
             <div>
               © <span>{new Date().getFullYear()}</span>
               <Link
