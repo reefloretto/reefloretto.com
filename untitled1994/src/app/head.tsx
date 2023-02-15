eaee081bbae2229ccf378111c8dcda4b15e29ea3
Reef Loretto <reefloretto@gmail.com>
2023-02-15 04:40:00 +0000

    add script

--- untitled1994/src/app/head.tsx
+++ untitled1994/src/app/head.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 export default function Head() {
   return (
     <>
@@ -5,6 +6,11 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Reef Loretto personal website" />
       <link rel="icon" href="/favicon.ico" />
+      <Script
+        defer
+        data-domain="reefloretto.com"
+        src="https://plausible.io/js/script.js"
+      />
     </>
   );
 }
