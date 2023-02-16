3feb3c711b82baa6c6a35b5497f6083148e47a57
Reef Loretto <reefloretto@gmail.com>
2023-02-16 03:11:58 +0000

    hide blog list

--- untitled1994/src/app/blog/page.tsx
+++ untitled1994/src/app/blog/page.tsx
@@ -1,9 +1,5 @@
 import Content from "./page.mdx";
 
 export default function Page() {
-  return (
-    <div className="font-semibold text-neutral-700 italic">
-      <Content />
-    </div>
-  );
+  return <Content />;
 }
