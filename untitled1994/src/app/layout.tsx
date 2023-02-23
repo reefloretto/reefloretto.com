7245ac6f809c0f3636e22a93e601950b2bf6ff1a
Reef Loretto <reefloretto@gmail.com>
2023-02-23 02:01:02 +0000

    add ellipsis

--- untitled1994/src/app/layout.tsx
+++ untitled1994/src/app/layout.tsx
@@ -1,3 +1,5 @@
+"use client"
+import { useState } from 'react'
 import Link from "next/link";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
@@ -9,6 +11,7 @@ import appleMusicIcon from "./images/applemusic.svg";
 import soundCloudIcon from "./images/soundcloud.svg";
 import bandcampIcon from "./images/bandcamp.svg";
 import mailIcon from "./images/mail.svg";
+import ellipsis from "./images/ellipsis.svg";
 import "./globals.css";
 
 const font = Inter({
@@ -21,6 +24,7 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const [showMoreLinks, setShowMoreLinks] = useState(false)
   const linkHoverClassName = "hover:opacity-20 transition-all";
   const iconHoverClassName =
     "opacity-30 hover:opacity-90 active:opacity-10 transition-all";
@@ -55,13 +59,6 @@ export default function RootLayout({
             <Link
               className={`${iconHoverClassName}}`}
               target="_blank"
-              href="https://twitter.com/reefloretto"
-            >
-              <Image alt="twitter" src={twitterIcon} width={24} height={24} />
-            </Link>
-            <Link
-              className={`${iconHoverClassName}}`}
-              target="_blank"
               href="https://www.instagram.com/reefloretto/"
             >
               <Image
@@ -74,55 +71,72 @@ export default function RootLayout({
             <Link
               className={`${iconHoverClassName}}`}
               target="_blank"
-              href="https://open.spotify.com/artist/7G7ksSJ4R5bwi7DqW4HkXE?si=g7aHrQUjTSqIW7LqqKt7Rg"
+              href="https://twitter.com/reefloretto"
             >
-              <Image alt="spotify" src={spotifyIcon} width={24} height={24} />
+              <Image alt="twitter" src={twitterIcon} width={24} height={24} />
             </Link>
-            <Link
-              className={`${iconHoverClassName}}`}
-              target="_blank"
-              href="https://music.apple.com/us/artist/reef-loretto/654440007"
-            >
-              <Image
-                alt="applemusic"
-                src={appleMusicIcon}
-                width={24}
-                height={24}
-              />
-            </Link>
-            <Link
-              className={`${iconHoverClassName}}`}
-              target="_blank"
-              href="https://soundcloud.com/reefloretto"
-            >
-              <Image
-                alt="soundcloud"
-                src={soundCloudIcon}
-                width={24}
-                height={24}
-              />
-            </Link>
-            <Link
-              className={`${iconHoverClassName}}`}
-              target="_blank"
-              href="https://reefloretto.bandcamp.com/"
-            >
-              <Image alt="bandcamp" src={bandcampIcon} width={24} height={24} />
-            </Link>
-            <Link
-              className={`${iconHoverClassName}}`}
-              target="_blank"
-              href="https://www.linkedin.com/in/reefloretto/"
-            >
-              <Image alt="linkedin" src={linkedInIcon} width={24} height={24} />
-            </Link>
-            <Link
-              className={`${iconHoverClassName}}`}
-              target="_blank"
-              href="mailto:r@reefloretto.com"
-            >
-              <Image alt="mail" src={mailIcon} width={24} height={24} />
-            </Link>
+            {showMoreLinks && (
+              <>
+                <Link
+                  className={`${iconHoverClassName}}`}
+                  target="_blank"
+                  href="https://open.spotify.com/artist/7G7ksSJ4R5bwi7DqW4HkXE?si=g7aHrQUjTSqIW7LqqKt7Rg"
+                >
+                  <Image alt="spotify" src={spotifyIcon} width={24} height={24} />
+                </Link>
+                <Link
+                  className={`${iconHoverClassName}}`}
+                  target="_blank"
+                  href="https://music.apple.com/us/artist/reef-loretto/654440007"
+                >
+                  <Image
+                    alt="applemusic"
+                    src={appleMusicIcon}
+                    width={24}
+                    height={24}
+                  />
+                </Link>
+                <Link
+                  className={`${iconHoverClassName}}`}
+                  target="_blank"
+                  href="https://soundcloud.com/reefloretto"
+                >
+                  <Image
+                    alt="soundcloud"
+                    src={soundCloudIcon}
+                    width={24}
+                    height={24}
+                  />
+                </Link>
+                <Link
+                  className={`${iconHoverClassName}}`}
+                  target="_blank"
+                  href="https://reefloretto.bandcamp.com/"
+                >
+                  <Image alt="bandcamp" src={bandcampIcon} width={24} height={24} />
+                </Link>
+                <Link
+                  className={`${iconHoverClassName}}`}
+                  target="_blank"
+                  href="https://www.linkedin.com/in/reefloretto/"
+                >
+                  <Image alt="linkedin" src={linkedInIcon} width={24} height={24} />
+                </Link>
+                <Link
+                  className={`${iconHoverClassName}}`}
+                  target="_blank"
+                  href="mailto:r@reefloretto.com"
+                >
+                  <Image alt="mail" src={mailIcon} width={24} height={24} />
+                </Link>
+              </>
+            )}
+            <div className={`${iconHoverClassName} cursor-pointer`} onClick={() => {
+              setShowMoreLinks(!showMoreLinks)
+            }}>
+              <Image alt="more" src={ellipsis} width={24} height={24} />
+            </div>
+
           </div>
           <footer className="font-extralight text-sm text-neutral-500 my-4">
             <div>
